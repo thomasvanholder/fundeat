3c72adc0fcc808b40453cd0e9b7c92ac61c0a3fa
hernan masaji kina <kisiro@gmail.com>
2020-03-02 17:02:33 -0300

    enum2

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,5 +4,6 @@ class Investment < ApplicationRecord
   belongs_to :reward_id
 
   # This enumerable saves integer to database but retrieves the symbol in the model
-  enum status: { pending: 0, on_transit: 1, delivered: 3, canceled: 4, refunded: 5 }
+  enum status: { pending: 0, on_transit: 1, delivered: 2, canceled: 3, refunded: 4 }
 end
+
