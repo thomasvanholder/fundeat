0362cecb2966fd37a7640469b67569dd30a908ae
hernan masaji kina <kisiro@gmail.com>
2020-03-02 16:38:00 -0300

    companymodel

---
+++ db/migrate/20200302193735_create_companies.rb
@@ -0,0 +1,19 @@
+class CreateCompanies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :type
+      t.string :address
+      t.float :longitude
+      t.float :latitude
+      t.integer :num_employees
+      t.string :instagram_url
+      t.string :tripadvisor_url
+      t.string :googlereview_url
+      t.string :description
+      t.references :owner, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
