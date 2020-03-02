6fef8d9bde01a793e90b1fc13cc7f8d7dd4efb8c
hernan masaji kina <kisiro@gmail.com>
2020-03-02 17:01:52 -0300

    enum

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
-  belongs_to :owner
+  belongs_to :owner, class_name: "User" # as we did with spotify project. check
+
+  # To use this array in views call it Company::TYPE
+  TYPE = ["Restaurant", "Coffe Shop", "Bar"]
+
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,4 +2,7 @@ class Investment < ApplicationRecord
   belongs_to :investor
   belongs_to :campaign_id
   belongs_to :reward_id
+
+  # This enumerable saves integer to database but retrieves the symbol in the model
+  enum status: { pending: 0, on_transit: 1, delivered: 3, canceled: 4, refunded: 5 }
 end
