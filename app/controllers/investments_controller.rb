8a401649b5b230dcfad788f1a41fd9b0b710f44f
Thomas Van Holder <thomasvanholder@gmail.com>
2020-03-03 13:09:39 -0300

    invest controller updated to submit form

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -8,8 +8,26 @@ class InvestmentsController < ApplicationController
   end
 
   def new
+    @campaign = Campaign.find(params[:capaign_id])
+    @investment = Investment.new
   end
 
   def create
+    @investment = Investment.new(investment_params)
+    authorize @investment
+    @campaign = Campaign.find(params[:campaign_id])
+    @investment.investor = current_user
+    if @investment.valid?
+      @investment.save
+      redirect_to new_campaign_investment_path(@investment)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def investment_params
+    params.require(:investment).permit(:amount, :)
   end
 end
