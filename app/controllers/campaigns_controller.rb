091d6f6f7cc83bba28c72385caa632c3981290aa
Thomas Van Holder <thomasvanholder@gmail.com>
2020-03-03 11:47:04 -0300

    changed investment links into campaigns links

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,6 +2,8 @@ class CampaignsController < ApplicationController
  #Note! pundit was implemented. remember to authorize your variables with for example. authorize@campaign
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
+    @campaings = policy_scope(Campaign)
+    authorize @campaings
   end
 
   def show
