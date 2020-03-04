2ed1f3eac8606e78ec66062f661252e128b835ce
Julius Ahlert <juliusahlert@gmail.com>
2020-03-04 20:02:41 -0300

    heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,12 +13,26 @@ rangeInput.addEventListener("click", (event) => {
   console.log(event.currentTarget);
 });
 
-const rangeInput = document.getElementById("investmentRange");
+const amountInput = document.getElementById("investment_amount");
 
-rangeInput.addEventListener("click", (event) => {
-  const rangeValue = document.querySelector("#investmentRange").value
-  const investmentAmount = document.querySelector("#investment_amount")
-  investmentAmount.value = rangeValue
-  console.log(rangeValue);
+amountInput.addEventListener("click", (event) => {
+  const investmentAmount = document.querySelector("#investment_amount").value
+  const rangeValue = document.querySelector("#investmentRange")
+  rangeValue.value = investmentAmount
+  console.log(investmentAmount);
   console.log(event.currentTarget);
+});
+
+flatpickr("#range_start", {
+  onChange: function(selectedDates, dateStr, instance) {
+
+    let pricePerDay = document.querySelector('.list-group-item').innerText.split("$")[1]
+
+    let totalPrice = days * pricePerDay
+
+    let span = document.querySelector('#pay_month');
+    let inputPrice = document.getElementById('booking_booking_price')
+    inputPrice.value = totalPrice
+    span.innerText = `${return_p_month}`
+  }
 });
