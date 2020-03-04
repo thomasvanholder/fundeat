01a23b3ea688e687ab1f022700a1541efbf3e6e2
Julius Ahlert <juliusahlert@gmail.com>
2020-03-04 11:59:17 -0300

    js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,11 @@ import "bootstrap";
 import { setWidth } from '../components/sticky_sidebar';
 
 setWidth();
+
+const rangeInput = document.getElementById("investmentRange");
+
+rangeInput.addEventListener("click", (event) => {
+  const rangeValue = document.querySelector("#investmentRange").value
+  console.log(rangeValue);
+  console.log(event.currentTarget);
+});
