988f94e76055ce80245d7212081a4fb8d05cd101
Julius Ahlert <juliusahlert@gmail.com>
2020-03-04 19:00:07 -0300

    upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,13 @@ rangeInput.addEventListener("click", (event) => {
   console.log(rangeValue);
   console.log(event.currentTarget);
 });
+
+const rangeInput = document.getElementById("investmentRange");
+
+rangeInput.addEventListener("click", (event) => {
+  const rangeValue = document.querySelector("#investmentRange").value
+  const investmentAmount = document.querySelector("#investment_amount")
+  investmentAmount.value = rangeValue
+  console.log(rangeValue);
+  console.log(event.currentTarget);
+});
