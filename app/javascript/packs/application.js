26f5a0f2aaf4093607cd55add846853767bef55a
Julius Ahlert <juliusahlert@gmail.com>
2020-03-04 14:45:44 -0300

    js slider

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ const rangeInput = document.getElementById("investmentRange");
 
 rangeInput.addEventListener("click", (event) => {
   const rangeValue = document.querySelector("#investmentRange").value
+  const investmentAmount = document.querySelector("#investment_amount")
+  investmentAmount.value = rangeValue
   console.log(rangeValue);
   console.log(event.currentTarget);
 });
