9b4ada810ef981fb760194e7dcb08bb6762b2bfd
hernan masaji kina <kisiro@gmail.com>
2020-03-05 00:01:52 -0300

    two graphs inserted

--- app/javascript/components/init_chart.js
+++ app/javascript/components/init_chart.js
@@ -3,6 +3,9 @@ import Chart from 'chart.js';
 const barCanvas = document.getElementById('bar');
 const pieCanvas = document.getElementById('pie');
 
+// barCanvas.canvas.width = 100;
+// barCanvas.canvas.height = 80;
+
 const dataBar = barCanvas.dataset.graph
 const dataPie = pieCanvas.dataset.graph
 
@@ -22,20 +25,13 @@ const initCharts = () => {
       }
     });
   }
-
+// For a pie chart
   if (pieCanvas) {
     new Chart(pieCanvas, {
-      type: 'bar',
+      type: 'pie',
       data: JSON.parse(dataPie),
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
+      // options: options,
+  //     data: JSON.parse(dataPie),
     });
   }
 
