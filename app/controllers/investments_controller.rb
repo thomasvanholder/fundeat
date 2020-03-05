a1b312308e9acc7b92154f6ec840fe6fa2e4f03e
hernan masaji kina <kisiro@gmail.com>
2020-03-05 12:29:55 -0300

    new commit

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -10,19 +10,66 @@ class InvestmentsController < ApplicationController
     @investments = Investment.where(investor_id: current_user.id)
     authorize @investments
 
+
     sum = 0
-    count = 0
+    count =[]
+    amount_by_proj = []
+    labels_proj = []
+
     @investments.each do |inv|
       sum += inv.campaign.return_rate
       count += 1
+
+      amount_by_proj << inv.amount
+      labels_proj << inv.campaign.company
+      raise
+
     end
-    # raise
     @avge_int_rate = sum/count
+
+    @investments.
 
     @graph_data = {
               labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
               datasets: [{
-                  label: '$ Investment / Month',
+                  label: 'Investment / Month',
+                  data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                  ],
+                  borderWidth: 1
+              }]
+          }
+
+    @graph_data_pie = {
+              labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+              datasets: [{
+                  label: "Investment %% by Project",
                   data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
                   backgroundColor: [
                       'rgba(255, 99, 132, 0.2)',
