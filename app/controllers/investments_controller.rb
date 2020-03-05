79ada0cc00713503d23df73ed4f5414cde8d86c7
hernan masaji kina <kisiro@gmail.com>
2020-03-05 15:01:40 -0300

    dynamics graphs.still to be improved.

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,3 +1,5 @@
+require "pry-byebug"
+
 class InvestmentsController < ApplicationController
   # attr_reader: investment
 
@@ -10,30 +12,30 @@ class InvestmentsController < ApplicationController
     @investments = Investment.where(investor_id: current_user.id)
     authorize @investments
 
-
     sum = 0
-    count =[]
+    count = 0
     amount_by_proj = []
     labels_proj = []
+    labels_months_of_invest = []
 
     @investments.each do |inv|
       sum += inv.campaign.return_rate
       count += 1
-
       amount_by_proj << inv.amount
-      labels_proj << inv.campaign.company
-      raise
+      labels_proj << inv.campaign.company.name
+      labels_months_of_invest << (inv.payment_date).strftime("%m/%d/%Y")
 
+      # binding.pry
     end
+
     @avge_int_rate = sum/count
-
-    @investments.
-
-    @graph_data = {
-              labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+    @graph_data_pie = {
+              # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+              labels: labels_proj,
               datasets: [{
                   label: 'Investment / Month',
-                  data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+                  # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+                  data: amount_by_proj,
                   backgroundColor: [
                       'rgba(255, 99, 132, 0.2)',
                       'rgba(54, 162, 235, 0.2)',
@@ -66,42 +68,44 @@ class InvestmentsController < ApplicationController
               }]
           }
 
-    @graph_data_pie = {
-              labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
-              datasets: [{
-                  label: "Investment %% by Project",
-                  data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                  ],
-                  borderWidth: 1
-              }]
-          }
+   @graph_data_bar = {
+      # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+      labels: labels_months_of_invest,
+      datasets: [{
+          label: 'Investment / Month',
+          # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+          data: amount_by_proj,
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 1
+      }]
+  }
   end
 
   def show
