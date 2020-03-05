4824fe5e32c4138164c8c6bb666d70b246d7d1f5
Julius Ahlert <juliusahlert@gmail.com>
2020-03-05 10:51:31 -0300

    spare file deleted

--- app/javascript/packs/investment_slider.js
+++
@@ -1,7 +0,0 @@
-const rangeInput = document.getElementById("rangeinput");
-
-rangeInput.addEventListener("click", (event) => {
-  const rangeValue = document.querySelector("#investmentRange").value
-  console.log(rangeValue);
-  console.log(event.currentTarget);
-});
