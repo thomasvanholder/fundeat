e0ece5dcff1368d54bf7fe24fdb5a5767a13c3fe
Julius Ahlert <juliusahlert@gmail.com>
2020-03-06 16:39:56 -0300

    Heroku app js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ import { setWidth } from '../components/sticky_sidebar';
 import '../components/return_calculation';
 import { initCharts } from '../components/init_chart';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
+import 'stylesheets/application'
 
 initMapbox();
 setWidth();
