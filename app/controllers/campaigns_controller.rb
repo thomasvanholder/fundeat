ac2b7962994a16e89b11a87479a5408321c5c4ef
Thomas Van Holder <thomasvanholder@gmail.com>
2020-03-06 18:15:38 -0300

    rewards created

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -50,7 +50,7 @@ class CampaignsController < ApplicationController
     @campaign.description = params[:campaign][:description]
     @campaign.loan_duration = params[:campaign][:loan_duration]
 
-  # seeded data
+    # seeded data
     @campaign.repayment_capacity = ("A".."C").to_a.sample
     @campaign.financial_health = ("A".."C").to_a.sample
     @campaign.company_history = ("A".."C").to_a.sample
@@ -60,81 +60,101 @@ class CampaignsController < ApplicationController
 
     @campaign.company = @company
 
-
-    # loop missing !!
-    @reward = Reward.new
+    reward = Reward.new
     authorize = reward
-    @reward.investment_amount = params[:reward][:investment_amount]
-    @reward.description = params[:reward][:description]
-    raise
-    # raise
-    if @campaign.save
-      redirect_to mycampaigns_path, notice: 'Campaign was successfully created.'
-    else
-      render :new
+    reward.investment_amount = params[:reward1][:investment_amount]
+    reward.description = params[:reward1][:description]
+    reward.campaign = @campaign
+    reward.save
+
+    reward = Reward.new
+    authorize = reward
+    reward.investment_amount = params[:reward2][:investment_amount]
+    reward.description = params[:reward2][:description]
+    reward.campaign = @campaign
+    reward.save
+
+    reward = Reward.new
+    authorize = reward
+    reward.investment_amount = params[:reward3][:investment_amount]
+    reward.description = params[:reward3][:description]
+    reward.campaign = @campaign
+    reward.save
+
+    reward = Reward.new
+    authorize = reward
+    reward.investment_amount = params[:reward4][:investment_amount]
+    reward.description = params[:reward4][:description]
+    reward.campaign = @campaign
+    reward.save
+      # raise
+      if @campaign.save
+        redirect_to mycampaigns_path, notice: 'Campaign was successfully created.'
+      else
+        render :new
+      end
     end
-  end
 
 
-  def edit
-  end
-
-  def update
-  end
-
-  def raising
-  @campaigns = policy_scope(Campaign)
-  authorize @campaigns
-end
-
-  def my_campaigns
-    @campaigns = Campaign.where(owner_id: current_user.id)
-    authorize @campaigns
-  end
-
-
-  def owners_dashboard
-    @campaigns = Campaign.where(owner_id: current_user.id)
-    authorize @campaigns
-  end
-
-  def mycampaigns
-    @campaign = Campaign.new
-    @company = Company.new
-    authorize @campaign
-    @campaigns = policy_scope(Campaign)
-    authorize @campaigns
-  end
-  def total_risk_level
-    char_array = []
-    char_array << @campaign.repayment_capacity
-    char_array << @campaign.financial_health
-    char_array << @campaign.company_history
-    string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
-    num_array = string.split(//)
-    sum = 0
-    num_array.each { |i| sum += i.to_i }
-    average = (sum / 3).round(0).to_s
-    @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
-  end
-
-  def dashboard
-    if current.user.owner?
-      :owner_dashboard
-    else
-      render :investor_dashboard
+    def edit
     end
+
+    def update
+    end
+
+    def raising
+      @campaigns = policy_scope(Campaign)
+      authorize @campaigns
+    end
+
+    def my_campaigns
+      @campaigns = Campaign.where(owner_id: current_user.id)
+      authorize @campaigns
+    end
+
+
+    def owners_dashboard
+      @campaigns = Campaign.where(owner_id: current_user.id)
+      authorize @campaigns
+    end
+
+    def mycampaigns
+      @campaign = Campaign.new
+      @company = Company.new
+      authorize @campaign
+      @campaigns = policy_scope(Campaign)
+      authorize @campaigns
+    end
+    def total_risk_level
+      char_array = []
+      char_array << @campaign.repayment_capacity
+      char_array << @campaign.financial_health
+      char_array << @campaign.company_history
+      string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
+      num_array = string.split(//)
+      sum = 0
+      num_array.each { |i| sum += i.to_i }
+      average = (sum / 3).round(0).to_s
+      @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
+    end
+
+    def dashboard
+      if current.user.owner?
+        :owner_dashboard
+      else
+        render :investor_dashboard
+      end
+    end
+
+    def support
+      @campaigns = policy_scope(Campaign)
+      authorize @campaigns
+    end
+
+    private
+
+    def campaign_params
+      params.require(:campaign).permit(:title)
+    end
+
   end
-
-  def support
-    @campaigns = policy_scope(Campaign)
-    authorize @campaigns
-  end
-
-  private
-
-  def campaign_params
-    params.require(:campaign).permit(:title)
-  end
-
-end
