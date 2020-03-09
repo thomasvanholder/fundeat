43bf106452eed8447ed984b50c659a5262319ee7
Julius Ahlert <juliusahlert@gmail.com>
2020-03-09 20:36:37 -0300

    css still fucked up but pushed

--- app/mailers/reward_mailer.rb
+++ app/mailers/reward_mailer.rb
@@ -5,6 +5,8 @@ class RewardMailer < ApplicationMailer
   #
   #   en.reward_mailer.confirmation.subject
   #
+  default from: 'rewards@fundeat.club'
+
   def confirmation
     @user = params[:user] # Instance variable => available in view
     mail(to: @user.email, subject: 'Your reward voucher')
