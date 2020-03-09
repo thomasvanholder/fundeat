c1b376603115df8ee885fe36b033b89a7fc6d22b
Hank Hng <hankhng@hotmail.com>
2020-03-09 16:07:49 -0300

    Owners dashboard created

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -113,48 +113,151 @@ class CampaignsController < ApplicationController
 
 
     def owners_dashboard
-      @company = current_user.companies.first
-      authorize @company
-    end
-
-    def mycampaigns
-      @campaign = Campaign.new
-      @company = Company.new
-      authorize @campaign
-      @campaigns = policy_scope(Campaign)
-      authorize @campaigns
-    end
-
-    def total_risk_level
-      char_array = []
-      char_array << @campaign.repayment_capacity
-      char_array << @campaign.financial_health
-      char_array << @campaign.company_history
-      string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
-      num_array = string.split(//)
-      sum = 0
-      num_array.each { |i| sum += i.to_i }
-      average = (sum / 3).round(0).to_s
-      @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
-    end
-
-    def dashboard
-      if current.user.owner?
-        :owner_dashboard
+      if current_user.companies.first.nil?
+        redirect_to mycampaigns_path, notice: 'Nothing to show, create your first campaign!'
+        @campaigns = policy_scope(Campaign)
+        authorize @campaigns
+        # render :mycampaigns
       else
-        render :investor_dashboard
-      end
+
+        @company = current_user.companies.first
+        authorize @company
+        @investments = @company.investments
+
+        sum = 0
+        count = 0
+        amount_by_proj = []
+        labels_proj = []
+        labels_months_of_invest = []
+
+        @investments.where.not(payment_date: nil).each do |inv|
+          sum += inv.campaign.return_rate
+          count += 1
+          amount_by_proj << inv.amount
+          labels_proj << inv.investor.first_name
+
+          labels_months_of_invest << (inv.payment_date)&.strftime("%m/%d/%Y")
+        end
+
+        @avge_int_rate = sum/count
+        @graph_data_pie = {
+              # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+              labels: labels_proj,
+              datasets: [{
+                label: 'Investment / Month',
+                  # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+                  data: amount_by_proj,
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                  ],
+                  borderWidth: 1
+                }]
+              }
+
+              @graph_data_bar = {
+      # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+      labels: labels_months_of_invest,
+      datasets: [{
+        label: 'Investment / Month',
+          # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+          data: amount_by_proj,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 1
+        }]
+      }
     end
-
-    def support
-      @campaigns = policy_scope(Campaign)
-      authorize @campaigns
-    end
-
-    private
-
-    def campaign_params
-      params.require(:campaign).permit(:title)
-    end
-
   end
+
+  def mycampaigns
+    @campaign = Campaign.new
+    @company = Company.new
+    authorize @campaign
+    @campaigns = policy_scope(Campaign)
+    authorize @campaigns
+  end
+
+  def total_risk_level
+    char_array = []
+    char_array << @campaign.repayment_capacity
+    char_array << @campaign.financial_health
+    char_array << @campaign.company_history
+    string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
+    num_array = string.split(//)
+    sum = 0
+    num_array.each { |i| sum += i.to_i }
+    average = (sum / 3).round(0).to_s
+    @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
+  end
+
+  def dashboard
+    if current.user.owner?
+      :owner_dashboard
+    else
+      render :investor_dashboard
+    end
+  end
+
+  def support
+    @campaigns = policy_scope(Campaign)
+    authorize @campaigns
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:title)
+  end
+
+end
--- app/models/company.rb
+++ app/models/company.rb
@@ -13,4 +13,8 @@ class Company < ApplicationRecord
     investments.where.not(payment_date: nil).sum(:amount)
   end
 
+  def number_of_investors
+    investments.where.not(payment_date: nil).count(:investor_id)
+  end
+
 end
