1f71444d8c11dbf095d09bcf3e16bd6b79250f64
Hank Hng <hankhng@hotmail.com>
2020-03-09 19:19:29 -0300

    Dashboard campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -63,10 +63,24 @@ class CampaignsController < ApplicationController
       authorize @campaigns
     end
 
-    def my_campaigns
-      @campaigns = Campaign.where(owner_id: current_user.id)
+    def mycampaigns
+      @campaigns = current_user.campaigns
       authorize @campaigns
+
+      if @campaigns.empty?
+        redirect_to new_campaign_path
+      end
+
     end
+
+
+    # def mycampaigns
+    #   @campaign = Campaign.new
+    #   @company = Company.new
+    #   authorize @campaign
+    #   @campaigns = policy_scope(Campaign)
+    #   authorize @campaigns
+    # end
 
 
     def owners_dashboard
@@ -95,9 +109,90 @@ class CampaignsController < ApplicationController
 
           labels_months_of_invest << (inv.payment_date)&.strftime("%m/%d/%Y")
         end
+        if @avge_int_rate.nil?
+          @graph_data_pie = {
+              # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+              labels: labels_proj,
+              datasets: [{
+                label: 'Investment / Month',
+                  # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+                  data: amount_by_proj,
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                  ],
+                  borderWidth: 1
+                }]
+              }
 
-        @avge_int_rate = sum/count
-        @graph_data_pie = {
+              @graph_data_bar = {
+      # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
+      labels: labels_months_of_invest,
+      datasets: [{
+        label: 'Investment / Month',
+          # data: [12, 19, 3, 5, 2, 3, 5, 6, 12, 3, 23, 12],
+          data: amount_by_proj,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+          ],
+          borderWidth: 1
+        }]
+      }
+
+
+    else
+      @avge_int_rate = sum/count
+
+      @graph_data_pie = {
               # labels: ['Jan', 'Feb', 'March', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'],
               labels: labels_proj,
               datasets: [{
@@ -176,89 +271,82 @@ class CampaignsController < ApplicationController
       }
     end
   end
+end
 
-  def mycampaigns
-    @campaign = Campaign.new
-    @company = Company.new
-    authorize @campaign
-    @campaigns = policy_scope(Campaign)
-    authorize @campaigns
+def total_risk_level
+  char_array = []
+  char_array << @campaign.repayment_capacity
+  char_array << @campaign.financial_health
+  char_array << @campaign.company_history
+  string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
+  num_array = string.split(//)
+  sum = 0
+  num_array.each { |i| sum += i.to_i }
+  average = (sum / 3).round(0).to_s
+  @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
+end
+
+def dashboard
+  if current.user.owner?
+    :owner_dashboard
+  else
+    render :investor_dashboard
   end
+end
 
-  def total_risk_level
-    char_array = []
-    char_array << @campaign.repayment_capacity
-    char_array << @campaign.financial_health
-    char_array << @campaign.company_history
-    string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
-    num_array = string.split(//)
-    sum = 0
-    num_array.each { |i| sum += i.to_i }
-    average = (sum / 3).round(0).to_s
-    @campaign.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
+def support
+  @campaigns = policy_scope(Campaign)
+  authorize @campaigns
+end
+
+def create_company
+  @company = Company.new
+  authorize @company
+  @company.owner = current_user
+  @company.address = params[:company]["address"]
+  @company.name = params[:company]["name"]
+  @company.num_employees = params[:company]["num_of_employees"]
+  @company.description = params[:company]["description"]
+end
+
+
+def create_campaign
+  @campaign = Campaign.new
+  authorize @campaign
+  @campaign.title = params[:campaign][:title]
+  @campaign.description = params[:campaign][:description]
+  @campaign.min_target = params[:campaign][:min_target]
+  @campaign.description = params[:campaign][:description]
+  @campaign.loan_duration = params[:campaign][:loan_duration]
+  @campaign.company = @company
+  seed_campaign_data
+end
+
+def create_rewards
+  (1..4).each do |reward_number|
+    next if (params["reward#{reward_number}".to_sym][:investment_amount]).blank?
+    reward = Reward.new
+    authorize = reward
+    reward.investment_amount = params["reward#{reward_number}".to_sym][:investment_amount]
+    reward.description = params["reward#{reward_number}".to_sym][:description]
+    reward.campaign = @campaign
+    reward.save
   end
+end
 
-  def dashboard
-    if current.user.owner?
-      :owner_dashboard
-    else
-      render :investor_dashboard
-    end
-  end
+def seed_campaign_data
+  @campaign.repayment_capacity = ("A".."C").to_a.sample
+  @campaign.financial_health = ("A".."C").to_a.sample
+  @campaign.company_history = ("A".."C").to_a.sample
+  @campaign.return_rate = rand(0.05..0.1).round(1)
+  @campaign.expiry_date = Date.today + rand(5..30).days
+  @campaign.risk_level = total_risk_level
+end
 
-  def support
-    @campaigns = policy_scope(Campaign)
-    authorize @campaigns
-  end
+private
 
-  def create_company
-    @company = Company.new
-    authorize @company
-    @company.owner = current_user
-    @company.address = params[:company]["address"]
-    @company.name = params[:company]["name"]
-    @company.num_employees = params[:company]["num_of_employees"]
-    @company.description = params[:company]["description"]
-  end
-
-
-  def create_campaign
-    @campaign = Campaign.new
-    authorize @campaign
-    @campaign.title = params[:campaign][:title]
-    @campaign.description = params[:campaign][:description]
-    @campaign.min_target = params[:campaign][:min_target]
-    @campaign.description = params[:campaign][:description]
-    @campaign.loan_duration = params[:campaign][:loan_duration]
-    @campaign.company = @company
-    seed_campaign_data
-  end
-
-  def create_rewards
-    (1..4).each do |reward_number|
-      next if (params["reward#{reward_number}".to_sym][:investment_amount]).blank?
-      reward = Reward.new
-      authorize = reward
-      reward.investment_amount = params["reward#{reward_number}".to_sym][:investment_amount]
-      reward.description = params["reward#{reward_number}".to_sym][:description]
-      reward.campaign = @campaign
-      reward.save
-    end
-  end
-
-  def seed_campaign_data
-    @campaign.repayment_capacity = ("A".."C").to_a.sample
-    @campaign.financial_health = ("A".."C").to_a.sample
-    @campaign.company_history = ("A".."C").to_a.sample
-    @campaign.return_rate = rand(0.05..0.1).round(1)
-    @campaign.expiry_date = Date.today + rand(5..30).days
-    @campaign.risk_level = total_risk_level
-  end
-
-  private
-
-  def campaign_params
-    params.require(:campaign).permit(:title)
-  end
+def campaign_params
+  params.require(:campaign).permit(:title)
+end
 
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,17 @@ class Campaign < ApplicationRecord
   belongs_to :company
   has_many :investments
   has_many :rewards
-
+  #belongs_to :owner, class_name: "User"
   def raised_sum
     self.investments.reduce(0) { |acum, investment| acum + investment.amount }
   end
+
+  def progress_percentage
+    if min_target.to_f == 0
+      return 0
+    else
+      ((raised_sum.to_f / min_target.to_f) * 100).round(0)
+    end
+  end
 end
+
