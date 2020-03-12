deb1b91ecdd6dbe3be9841c943c8d512f2932b31
Hank Hng <hankhng@hotmail.com>
2020-03-12 16:47:15 -0300

    flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,8 @@ import { setWidth } from '../components/sticky_sidebar';
 import '../components/return_calculation';
 import { initCharts } from '../components/init_chart';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-import "../plugins/countdown";
 import "../plugins/flatpickr";
-// import { initCountdown } from '../components/init_countdown';
+import "../plugins/countdown";
 require("chartkick")
 require("chart.js")
 
