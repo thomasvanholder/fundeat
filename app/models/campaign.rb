458f94db5c625c907db826c935c6afe2f8311ce0
hernan masaji kina <kisiro@gmail.com>
2020-03-12 14:47:16 -0300

    showdone

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -21,5 +21,26 @@ class Campaign < ApplicationRecord
       ((raised_sum.to_f / min_target.to_f) * 100).round(0)
     end
   end
+
+  def expired?
+    ((expiry_date - Time.now)/86400) < 0
+  end
+
+
+ #Note HK - Risklevel: this way risk level is shown in the show page. (i am just commenting it as Julius will be trying to fix it directly on the database)
+   # def total_risk_level
+  #   char_array = []
+  #   char_array << repayment_capacity
+  #   char_array << financial_health
+  #   char_array << company_history
+  #   string = char_array.join.gsub!(/[ABC]/, 'A' => 1, 'B' => 2, 'C' => 3)
+  #   num_array = string.split(//)
+  #   sum = 0
+  #   num_array.each { |i| sum += i.to_i }
+  #   average = (sum / 3).round(0).to_s
+  #   self.risk_level = average.gsub!(/[123]/, '1' => 'A', '2' => 'B', '3' => 'C')
+
+  # end
+
 end
 
