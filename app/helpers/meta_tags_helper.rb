94a297d530f5fadf36849f4a7c1b507f24e4c597
Thomas Van Holder <thomasvanholder@gmail.com>
2020-03-12 13:26:07 -0300

    added

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["FUND your local places where you enjoy to EAT"]
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Fundeat enables Restaurants to easily raise money and gives private investors the opportunity to sustainably invest in their local community"]
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["https://images.unsplash.com/photo-1556910103-1c02745aae4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"])
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
