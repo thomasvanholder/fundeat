79a47699df23ad406ab45049ac1c3a7cbb897871
Hank Hng <hankhng@hotmail.com>
2020-03-12 11:38:11 -0300

    Confirmation page

---
+++ app/javascript/components/init_countdown.js
@@ -0,0 +1,16 @@
+import Countdown from 'countdown.js';
+
+// const initCountdown = () => {
+
+const efcc_countdown = new Countdown({
+  target: '.countdown',
+  dayWord: ' days',
+  hourWord: ' hours',
+  minWord: ' mins',
+  secWord: ' secs'
+});
+
+// }
+
+// export { initCountdown }
+
