5de0f23ce687c567395b34980ad2a8c0ea42d872
Hank Hng <hankhng@hotmail.com>
2020-03-13 15:28:13 -0300

    fix

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -62,31 +62,10 @@ class InvestmentsController < ApplicationController
                     'rgba(103, 219, 118, 0.4)',
                     'rgba(121, 170, 127, 0.6)',
                     'rgba(188, 225, 146, 0.8)',
-                    'rgba(188, 225, 146, 1)',
-                    'rgba(188, 200, 146, 0.2)',
-                    'rgba(103, 200, 118, 0.4)',
-                    'rgba(121, 200, 127, 0.6)',
-                    'rgba(188, 200, 146, 0.8)',
-                    'rgba(188, 200, 146, 1)',
-                    'rgba(100, 225, 146, 0.2)',
-                    'rgba(100, 219, 118, 0.4)',
-                    'rgba(100, 170, 127, 0.6)',
-                    'rgba(100, 225, 146, 0.8)',
-                    'rgba(100, 225, 146, 1)',
-                    'rgba(100, 120, 130, 0.2)',
-                    'rgba(100, 120, 130, 0.4)',
-                    'rgba(100, 120, 130, 0.6)',
-                    'rgba(100, 120, 130, 0.8)',
-                    'rgba(100, 120, 130, 1)',
-                    'rgba(188, 225, 146, 0.2)',
-                    'rgba(103, 219, 118, 0.4)',
-                    'rgba(121, 170, 127, 0.6)',
-                    'rgba(188, 225, 146, 0.8)',
-                    'rgba(188, 225, 146, 1)',
                   ],
                   borderColor: [
-                    # 'rgba(33, 127, 45, 1)',
-                    # 'rgba(33, 127, 45 1)',
+                    'rgba(33, 127, 45, 1)',
+                    'rgba(33, 127, 45 1)',
                   ],
                   borderWidth: 1
                 }]
@@ -102,7 +81,7 @@ class InvestmentsController < ApplicationController
           data: amount_by_proj,
           backgroundColor: [
             'rgba(188, 225, 146, 0.2)',
-            'rgba(150, 190, 132, 0.4)',
+            'rgba(150, 190, 132, 0.2)',
           ],
           borderColor: [
             'rgba(75, 192, 192, 1)',
@@ -215,17 +194,6 @@ class InvestmentsController < ApplicationController
     authorize @investment
   end
 
-
-
-  def dashboard
-    if current.user.owner?
-      :owner_dashboard
-    else
-      render :investor_dashboard
-    end
-  end
-
-
   def new
     @campaign = Campaign.find(params[:campaign_id])
     authorize @campaign
@@ -247,10 +215,12 @@ class InvestmentsController < ApplicationController
         line_items: [{
           name: @campaign.title,
           # images: @campaign.photo_url],
+
           amount: @investment.amount * 100,
           currency: 'usd',
           quantity: 1
         }],
+        # success_url: investments_url,
         success_url: investment_link,
         cancel_url: campaigns_url
         )
@@ -258,6 +228,7 @@ class InvestmentsController < ApplicationController
       @investment.update(stripe_session_id: session.id)
     else
       render :new
+
     end
   end
 
